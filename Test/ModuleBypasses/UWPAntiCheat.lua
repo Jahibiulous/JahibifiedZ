daf5ceeb57a1a2af3c73fb41649e566218c56fd5
Jahibiulous <146329085+Jahibiulous@users.noreply.github.com>
2023-09-28 11:07:58 +0200

    Update UWPAntiCheat.lua

--- Test/ModuleBypasses/UWPAntiCheat.lua
+++ Test/ModuleBypasses/UWPAntiCheat.lua
@@ -1,1 +1,14 @@
-o
+print("Loaded UWP + Anti Cheat Removal System")
+wait(1)
+
+local function getBit()
+    local address = tonumber(string.sub(tostring{math.huge},8))
+    if #tostring(address) <= 10 then
+        return 32
+    end
+    return 64
+end
+
+if getBit() == 32 then
+    print("bye uwp detector")
+end
